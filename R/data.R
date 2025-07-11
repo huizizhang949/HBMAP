1098599212d0c52f8a871e211fd13c0a69e84491
huizizhang949 <zhanghuizi98@outlook.com>
2025-07-11 10:41:44 +0100

    update data and demo

--- R/data.R
+++ R/data.R
@@ -1,12 +1,25 @@
 #' The visual cortex data
 #'
 #' @description
-#' This data describes the projections of single neurons from the primary visual cortex to the to the
-#' lateroitermediate (LI), lateromedial (LM), anterolateral (AL), posteromedial (PM), anteromedial (AM),
-#' and rostrolateral (RL) higher visual areas.
+#' The visual cortex data describes the projections of single neurons from the primary visual cortex to the
+#' laterointermediate (LI), lateromedial (LM), anterolateral (AL), posteromedial (PM), anteromedial (AM), and rostrolateral (RL) areas.
 #'
-#' @format A list of matrices for four mice. Each is a barcode-count matrix with rows for regions and columns for neurons.
+#' @format A list of matrices. Each list contains a barcode-count matrix from a mouse, with rows for regions and columns for neurons.
 #'
-#' @references Han, Y., Kebschull, J., Campbell, R. et al. The logic of single-cell projections from visual cortex. Nature 556, 51–56 (2018).
+#' @references Han, Y., Kebschull, J.M., Campbell, R.A. et al. The logic of single-cell projections from visual cortex. Nature 556, 51–56 (2018).
 "data_Hans"
+
+
+#' The auditory cortex data
+#'
+#' @description
+#' The auditory cortex data considers projections from the auditory cortex to the target regions:
+#' the orbitofrontal cortex (OFC), motor cortex (Motor), rostral striatum (Rstr), somatosensory cortex (SSctx),
+#' caudal striatum (Cstr), amygdala (Amyg), ipsilateral visual cortex (VisIp), contralateral visual cortex (VisC),
+#' contralateral auditory cortex (AudC), thalamus (Thal), and tectum (Tect).
+#'
+#' @format A list of matrices. Each list contains a barcode-count matrix from a mouse, with rows for regions and columns for neurons.
+#'
+#' @references Chen, X., Sun, Y.-C., Zhan, H. et al. High-throughput mapping of long-range neuronal projection using in situ sequencing. Cell 179, 772-786 (2019).
+"data_barseq"
 
